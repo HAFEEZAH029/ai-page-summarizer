0e51fe21e4d07dbf9f48fa6bc385f2970da6485b
HAFEEZAH029 <khafeezah2000@gmail.com>
2026-05-02 04:28:33 -1100

    project setup

---
+++ src/Popup/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: 16, width: 300 }}>
+      <h3>AI Page Summarizer</h3>
+      <button>Summarize Page</button>
+    </div>
+  );
+}
+
+export default App;
